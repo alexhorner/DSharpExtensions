587527aa1ab8aad1590b797ea8d251ca6477134a
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2019-12-28 09:40:21 +0000

    Add reflection utils, create client extensions
    Added the reflection utility for getting private and internal members from objects, and created a class for extension methods which go on the DiscordClient. Added CreateDmChannelAsync extension method

---
+++ DSharpExtensions/DiscordClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using DSharpExtensions.Internal;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Net;
+
+namespace DSharpExtensions
+{
+    public static class DiscordClientExtensions
+    {
+        public static async Task<DiscordDmChannel> CreateDmChannelAsync(this DiscordClient client, ulong id)
+        {
+            DiscordApiClient apiClient = ReflectionUtility.GetObjectFieldByType<BaseDiscordClient, DiscordApiClient>(client) as DiscordApiClient; //I know this isn't pretty.
+
+            MethodInfo createDmAsyncMethod = typeof(DiscordApiClient).GetMethod("CreateDmAsync", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return await (createDmAsyncMethod.Invoke(apiClient, new object[] { id }) as Task<DiscordDmChannel>);
+        }
+    }
+}
---
+++ DSharpExtensions/Internal/ReflectionUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DSharpExtensions.Internal
+{
+    internal static class ReflectionUtility
+    {
+        internal static object GetObjectFieldByType<TObj, TField>(object obj)
+        {
+            //I know this is horrid
+
+            FieldInfo[] fields = typeof(TObj).GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+            return fields.FirstOrDefault(f => f.FieldType == typeof(TField)).GetValue(obj);
+        }
+    }
+}
