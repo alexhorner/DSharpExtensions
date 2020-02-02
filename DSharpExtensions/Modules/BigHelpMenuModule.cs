3a945a58c029eaceeb6072224759dfd33297cda9
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2020-02-02 17:23:15 +0000

    BigHelpModule fix and clean
    Remove old code from the module, and fix empty categories causing an exception

--- DSharpExtensions/Modules/BigHelpMenuModule.cs
+++ DSharpExtensions/Modules/BigHelpMenuModule.cs
@@ -18,71 +18,6 @@ namespace DSharpExtensions.Modules
         public static DiscordColor EmbedColor = new DiscordColor(150, 150, 255);
         public static string HelpTitle = "Help Menu";
         public static string Description = null;
-
-        /*private static Dictionary<string, string> _helpCategories;
-
-        [Command("help")]
-        public async Task Help(CommandContext ctx)
-        {
-            DiscordEmbedBuilder helpEmbed = new DiscordEmbedBuilder
-            {
-                Color = EmbedColor,
-                Title = HelpTitle,
-                Description = Description
-            };
-
-            if (_helpCategories == null)
-            {
-                _helpCategories = new Dictionary<string, string>();
-
-                Dictionary<string, List<Command>> moduleCommands = new Dictionary<string, List<Command>>();
-
-                foreach (KeyValuePair<string, Command> command in ctx.CommandsNext.RegisteredCommands)
-                {
-                    if (command.Value.IsHidden || command.Value.Module.ModuleType.GetCustomAttribute(typeof(HiddenAttribute)) != null)
-                    {
-                        continue;
-                    }
-
-                    string moduleName = command.Value.Module.ModuleType.GetCustomAttribute(typeof(FriendlyName)) != null ? (command.Value.Module.ModuleType.GetCustomAttribute(typeof(FriendlyName)) as FriendlyName).Value : command.Value.Module.ModuleType.Name;
-
-                    if (!moduleCommands.ContainsKey(moduleName))
-                    {
-                        moduleCommands.Add(moduleName, new List<Command>());
-                    }
-
-                    if (moduleCommands.TryGetValue(moduleName, out List<Command> commands))
-                    {
-                        commands.Add(command.Value);
-                    }
-                }
-
-                foreach (KeyValuePair<string, List<Command>> module in moduleCommands)
-                {
-                    string finalCommands = "";
-
-                    foreach (Command command in module.Value)
-                    {
-                        if (finalCommands != "")
-                        {
-                            finalCommands += "\n";
-                        }
-
-                        finalCommands += command.Name + (!string.IsNullOrEmpty(command.Description) ? " - " + command.Description : "");
-                    }
-
-                    _helpCategories.Add(module.Key, finalCommands);
-                }
-            }
-
-            foreach (KeyValuePair<string, string> category in _helpCategories)
-            {
-                helpEmbed.AddField(category.Key, category.Value);
-            }
-
-            await ctx.RespondAsync(embed: helpEmbed);
-        }*/
-
         private static List<CategoryModel> _categories;
 
         [Command("help")]
@@ -153,7 +88,10 @@ namespace DSharpExtensions.Modules
                     categoryContent += command.Name + (!string.IsNullOrEmpty(command.Description) ? " - " + command.Description : "");
                 }
 
-                helpCategories.Add(category.Name, categoryContent);
+                if (categoryContent != "")
+                {
+                    helpCategories.Add(category.Name, categoryContent);
+                }
             }
 
             foreach (KeyValuePair<string, string> category in helpCategories)
